4a4e3f47e14ee8354d3a9be60a65fbb2ef330009
Nitish Kumar <nits.kr99@gmail.com>
2023-06-19 13:42:24 +0530

    done

--- src/components/Agent.js
+++ src/components/Agent.js
@@ -346,7 +346,7 @@ function Agent() {
                                               deleteAgent(agent._id)
                                             }
                                           >
-                                            <FontAwesomeIcon icon={faTrash} />
+                                            <FontAwesomeIcon icon={faTrash}/>
                                           </button>
                                         </span>
                                       </td>
